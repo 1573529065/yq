3e34a6bd6ac52d81cc86ec3eb98ef4f4e2f53f12
zhw <1573529065@qq.com>
2019-07-09 23:46:13 +0800

    课程管理

--- application/admin/controller/Train.php
+++ application/admin/controller/Train.php
@@ -9,15 +9,61 @@
 namespace app\admin\controller;
 
 
+use think\Db;
+use think\Error;
+
 class Train extends Admin
 {
 
     /**
      * 培训课程管理
+     * @return mixed
+     * @throws \think\exception\DbException
      */
     public function index()
     {
+        $list = Db::table('train')->where("status!=:status")->bind(['status'=>[-1,\PDO::PARAM_INT]])->paginate(10);
 
+        return $this->render('index', ['list' => $list]);
+    }
+
+    /**
+     * 添加培训课程
+     */
+    public function add()
+    {
+        if ($this->request->isPost()){
+
+        }
+
+        return $this->render('add');
+    }
+
+    /**
+     * 编辑课程
+     */
+    public function edit()
+    {
+        if ($this->request->isPost()){
+
+        }
+
+        $id = intval($this->request->get('id'));
+        if (empty($id)){
+            $this->error('缺少参数');
+        }
+        $info = Db::table('train')->where("id=:id", ['id' => [$id, \PDO::PARAM_INT]])->find();
+
+
+        return $this->render('edit', ['info' => $info]);
+    }
+
+    /**
+     * 删除课程
+     */
+    public function delete()
+    {
+        
     }
 
 }
